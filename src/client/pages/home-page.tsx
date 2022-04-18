17034574fb193891344bb603073ce41137368e3b
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2022-04-18 23:21:11 +1000

    services

--- src/client/pages/home-page.tsx
+++ src/client/pages/home-page.tsx
@@ -7,76 +7,75 @@ export const HomePage: React.FC = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column' }}>
       <Banner imagePath="DSC_0793.jpg" />
-      <Grid container style={{ marginTop: '4rem' }}>
-        <div
-          style={{
-            margin: 'auto',
-            padding: '10px',
-          }}
-        >
-          <h3> Brilliant Brickwork caters to the needs of:</h3>
-          <ul style={{ listStyleType: 'none', padding: '10px' }}>
-            <li>- The individual home owner</li>
-            <li>- Builders seeking Subcontractors</li>{' '}
-            <li>- Real estate agents/owners corporations</li>{' '}
-            <li>- Maintenance companies</li>
-            <li>- and more!</li>
-          </ul>
-        </div>
-        <img
-          src={`${imagePath}filename_22.jpg`}
-          alt="filename_22.jpg"
-          height="300"
-          width="500"
-          style={{ margin: 'auto', padding: '10px' }}
-        />
-      </Grid>
-      <Grid container style={{ marginTop: '4rem' }}>
-        <img
-          src={`${imagePath}48.jpg`}
-          alt="48.jpg"
-          height="300"
-          width="500"
-          style={{ margin: 'auto', padding: '10px' }}
-        />
-        <div
-          style={{
-            margin: 'auto',
-
-            padding: '10px',
-          }}
-        >
-          <ul style={{ listStyleType: 'none', padding: '10px' }}>
-            <li>
-              <h3>- Experienced and reliable</h3>
-            </li>
-            <li>
-              <h3>- All jobs big and small</h3>
-            </li>
-            <li>
-              <h3>- Specializing in all types of brick and block work</h3>
-            </li>
-          </ul>
-        </div>
-      </Grid>
-      <Grid container style={{ marginTop: '4rem' }}>
-        <div
-          style={{
-            margin: 'auto',
-
-            padding: '10px',
-          }}
-        >
-          <h3>Always professional, on-time & friendly!</h3>
-        </div>
-        <img
-          src={`${imagePath}DSC_0778.jpg`}
-          alt="DSC_0778.jpg"
-          height="300"
-          width="500"
-          style={{ margin: 'auto', padding: '10px' }}
-        />
-      </Grid>
+      <div className="tileHolder" style={{ margin: '4rem 12rem 0 12rem' }}>
+        <Grid container>
+          <div
+            style={{
+              margin: 'auto',
+              padding: '10px',
+              fontSize: 22,
+            }}
+          >
+            <h3> Brilliant Brickwork caters to the needs of:</h3>
+            <ul style={{ listStyleType: 'none', padding: '10px' }}>
+              <li>- The individual home owner</li>
+              <li>- Builders seeking Subcontractors</li>{' '}
+              <li>- Real estate agents/owners corporations</li>{' '}
+              <li>- Maintenance companies</li>
+              <li>- and more!</li>
+            </ul>
+          </div>
+          <img
+            src={`${imagePath}filename_22.jpg`}
+            alt="filename_22.jpg"
+            height="300"
+            width="500"
+            style={{ margin: 'auto', padding: '10px' }}
+          />
+        </Grid>
+        <Grid container style={{ marginTop: '4rem' }}>
+          <img
+            src={`${imagePath}48.jpg`}
+            alt="48.jpg"
+            height="300"
+            width="500"
+            style={{ margin: 'auto', padding: '10px' }}
+          />
+          <div
+            style={{
+              margin: 'auto',
+              fontSize: 22,
+              padding: '10px',
+            }}
+          >
+            <ul style={{ listStyleType: 'none', padding: '10px' }}>
+              <li>- Experienced and reliable</li>
+              <br />
+              <li>- All jobs big and small</li>
+              <br />
+              <li>- Specializing in all types of brick and block work</li>
+            </ul>
+          </div>
+        </Grid>
+        <Grid container style={{ marginTop: '4rem' }}>
+          <div
+            style={{
+              margin: 'auto',
+              fontSize: 22,
+              padding: '10px',
+            }}
+          >
+            <h3>Always professional, on-time & friendly!</h3>
+          </div>
+          <img
+            src={`${imagePath}DSC_0778.jpg`}
+            alt="DSC_0778.jpg"
+            height="300"
+            width="500"
+            style={{ margin: 'auto', padding: '10px' }}
+          />
+        </Grid>
+      </div>
       <Grid container style={{ marginTop: '4rem' }}>
         <Banner imagePath="filename_01.jpg" />
         <div
--- src/client/pages/services-page.tsx
+++ src/client/pages/services-page.tsx
@@ -6,7 +6,29 @@ const Services: React.FC = () => {
   return (
     <div>
       <BBHeader pageNo={pageID} />
-      <div style={{ display: 'flex', flexDirection: 'column' }}></div>
+      <div style={{ display: 'flex', flexDirection: 'column' }}>
+        <div
+          style={{
+            margin: 'auto',
+            padding: '10px',
+          }}
+        >
+          <h2> The different services which we provide:</h2>
+          <ul style={{ listStyleType: 'none', padding: '10px', fontSize: 22 }}>
+            <li>- Insurance work</li>
+            <li>- Repair work</li> <li>- New Houses</li>{' '}
+            <li>- Extensions & Renovations</li>
+            <li>- Boundary walls</li>
+            <li>- Fences</li>
+            <li>- Retaining walls</li>
+            <li>- Piers</li>
+            <li>- Garages</li>
+            <li>- Steps</li>
+            <li>- Sills</li>
+            <li>- Letterboxes and almost anything else!</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
